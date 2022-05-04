52a2229b3255033aaacee00bb54dfcad6388a80f
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-05 03:59:53 +0430

    swiper added

--- script.js
+++ script.js
@@ -16,7 +16,6 @@
 //       });
 //       item.classList.add("active");
 //       document.getElementById("specialties").style.border = "solid #014ed5";
-      
 
 //       // SOMEHOW EQUATE TAB LINKS TO TAB PANES
 //       console.log(e.target);
@@ -25,11 +24,62 @@
 //   );
 // });
 
-
 // $('.specialties-col').click(function() {
 // 	$('.specialties-col').style.border = "solid #014ed5";
 // 	// $(this).show();
-	
+
 // 	$('.specialties-col').removeClass('active');
 // 	$(this).addClass('active');
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 8,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// var swiper = new Swiper(".specialist-card", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// // configure Swiper to use modules
+// Swiper.use([Navigation, Pagination]);
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   grid: {
+//     rows: 2,
+//   },
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
 // });
