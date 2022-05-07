0de00c098ae3952a925cdfb186172f1f2723c9f6
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-07 22:04:46 +0430

    remove serchTool js added

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// function selected() {
-//   document.getElementById("specialties-col").style.border = "solid #014ed5";
-// }
 
+// swiper section
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 8,
   spaceBetween: 20,
@@ -17,3 +15,44 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+
+// remove searchTool
+function removeElement(){
+  document.getElementById("remove").remove();
+  // document.getElementById('remove').style.display = 'none'
+}
+
+
+
+// $(document).ready(function(){
+//   var activeCat = "";
+//   function filterGroup(group){
+//     if(activeCat != group){
+//       $("div").filter("."+group).show();
+//       $("div").filter(":not(."+group+")").hide();
+//       activeCat = group;
+//     }
+//   }
+  
+//   $(".cat-all").click(function(){
+//     $("div").show();
+//     activeCat = "all";
+//   });
+//   $(".cat-1").click(function(){ filterGroup("group-1"); });
+
+//   $(".cat-2").click(function(){ filterGroup("group-2"); });
+//   $(".cat-3").click(function(){ filterGroup("group-3"); });
+// });
+
+$(document).on('click', '.btn-cat', function(e){
+  var $type = $(this).data("cat-source");
+  if($type == "all"){
+    $('.portfolio-block').fadeOut(0);
+    $('.portfolio-block').fadeIn(1000);
+  }else{
+    $('.portfolio-block').hide();
+    $('#'+$type + ".portfolio-block").fadeIn(1000);
+  }
+})
