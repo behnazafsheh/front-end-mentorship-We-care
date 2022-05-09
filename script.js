f5197e2301c87aad6f27da68e5a55aa114ece8fb
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-09 16:48:19 +0430

    layout changed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // swiper section
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 8,
@@ -16,43 +15,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
-
 // remove searchTool
-function removeElement(){
+function removeElement() {
   document.getElementById("remove").remove();
-  // document.getElementById('remove').style.display = 'none'
 }
-
-
-
-// $(document).ready(function(){
-//   var activeCat = "";
-//   function filterGroup(group){
-//     if(activeCat != group){
-//       $("div").filter("."+group).show();
-//       $("div").filter(":not(."+group+")").hide();
-//       activeCat = group;
-//     }
-//   }
-  
-//   $(".cat-all").click(function(){
-//     $("div").show();
-//     activeCat = "all";
-//   });
-//   $(".cat-1").click(function(){ filterGroup("group-1"); });
-
-//   $(".cat-2").click(function(){ filterGroup("group-2"); });
-//   $(".cat-3").click(function(){ filterGroup("group-3"); });
-// });
-
-// $(document).on('click', '.btn-cat', function(e){
-//   var $type = $(this).data("cat-source");
-//   if($type == "all"){
-//     $('.portfolio-block').fadeOut(0);
-//     $('.portfolio-block').fadeIn(1000);
-//   }else{
-//     $('.portfolio-block').hide();
-//     $('#'+$type + ".portfolio-block").fadeIn(1000);
-//   }
-// })
