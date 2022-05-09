da1350189954943f080a108b62c7c5316ffe8ab1
behnazafsheh <behnazfaheh1992@gmail.com>
2022-05-09 20:55:13 +0430

    Specialist section changed

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
-// swiper section
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 8,
-//   spaceBetween: 20,
-//   slidesPerGroup: 1,
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 // remove searchTool
 function removeElement() {
   document.getElementById("remove").remove();
 }
-
-
-
-
-
-{/* <div class="a-box">
-  <div class="img-container">
-    <div class="img-inner">
-      <div class="inner-skew">
-        <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ">
-      </div>
-    </div>
-  </div>
-  <div class="text-container">
-    <h3>A blue bird</h3>
-    <div>
-      This a demo experiment to skew image container. It looks good.
-  </div>
-</div> */}
